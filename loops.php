45384829b29b466cce278c9ea5c36a5ebade5721
Sean Davis <sdavis2702@gmail.com>
2013-01-27 22:19:30 -0600

    Add custom filters

--- loops.php
+++ loops.php
@@ -99,11 +99,8 @@ function vol_content() {
 				
 				$article_tags_text = apply_filters( 'article_tags_text', 'Tags: ' );
 				
-				if ( $options_posts[ 'singletags' ] == 1 ) {
-					echo "{$tab3}<div class=\"entry-meta tags post-meta-footer\">\n", 
-					the_tags( __( $article_tags_text, 'volatyl' ), ', ', '<br />' );
-					echo "{$tab3}</div>\n";
-				}
+				if ( $options_posts[ 'singletags' ] == 1 )
+					the_tags( __( '<div class="entry-meta tags post-meta-footer">' . $article_tags_text, 'volatyl' ), ', ', '<br /></div>' );
 				
 				$post_page_nav = apply_filters( 'post_page_nav', 'Pages:' );
 			
@@ -189,6 +186,14 @@ function vol_content() {
 
 		// Archives including categories, tags, authors, dates, and bears. Oh my!
 		} elseif ( is_archive() ) {
+		
+			// Custom filters
+			$cat_header = apply_filters( 'cat_header', 'Category Archives:' );
+			$tag_header = apply_filters( 'tag_header', 'Tag Archives:' );
+			$author_header = apply_filters( 'author_header', 'Author Archives:' );
+			$daily_header = apply_filters( 'daily_header', 'Daily Archives:' );
+			$monthly_header = apply_filters( 'monthly_header', 'Monthly Archives:' );
+			$yearly_header = apply_filters( 'yearly_header', 'Yearly Archives:' );
 
 			// vol_before_content_column
 			$options_hooks = get_option( 'vol_hooks_options' );
@@ -199,10 +204,10 @@ function vol_content() {
 					"\t\t<h1 class=\"page-title\">";
 				
 			if ( is_category() ) {
-				printf( __( 'Category Archives: %s', 'volatyl' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+				printf( __( $cat_header . ' %s', 'volatyl' ), '<span>' . single_cat_title( '', false ) . '</span>' );
 
 			} elseif ( is_tag() ) {
-				printf( __( 'Tag Archives: %s', 'volatyl' ), '<span>' . single_tag_title( '', false ) . '</span>' );
+				printf( __( $tag_header . ' %s', 'volatyl' ), '<span>' . single_tag_title( '', false ) . '</span>' );
 
 			} elseif ( is_author() ) {
 	
@@ -210,7 +215,7 @@ function vol_content() {
 				// what author we're dealing with (if that is the case)
 				the_post();
 			
-				printf( __( 'Author Archives: %s', 'volatyl' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
+				printf( __( $author_header . ' %s', 'volatyl' ), '<span class="vcard"><a class="fn" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '">' . get_the_author() . '</a></span>' );
 			
 				// Since we called the_post() above, we need to
 				// rewind the loop back to the beginning that way
@@ -218,13 +223,13 @@ function vol_content() {
 				rewind_posts();
 			
 			} elseif ( is_day() ) {
-				printf( __( 'Daily Archives: %s', 'volatyl' ), '<span>' . get_the_date() . '</span>' );
+				printf( __( $daily_header . ' %s', 'volatyl' ), '<span>' . get_the_date() . '</span>' );
 
 			} elseif ( is_month() ) {
-				printf( __( 'Monthly Archives: %s', 'volatyl' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
+				printf( __( $monthly_header . ' %s', 'volatyl' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
 
 			} elseif ( is_year() ) {
-				printf( __( 'Yearly Archives: %s', 'volatyl' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
+				printf( __( $yearly_header . ' %s', 'volatyl' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
 
 			} else {
 				_e( 'Archives', 'volatyl' );
