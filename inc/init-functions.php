55d43871c846d741647e3f0f4e5a921264095970
Sean Davis <sdavis2702@gmail.com>
2014-05-09 23:19:35 -0500

    update version

--- inc/init-functions.php
+++ inc/init-functions.php
@@ -14,7 +14,7 @@
  
 // Constants
 define ('THEME_NAME', 'Volatyl');
-define ('THEME_VERSION', '1.4.1');
+define ('THEME_VERSION', '1.4.2');
 define ('THEME_URI', 'http://volatylthemes.com');
 define ('THEME_PATH', get_template_directory());
 define ('THEME_PATH_CHILD', get_stylesheet_directory());
