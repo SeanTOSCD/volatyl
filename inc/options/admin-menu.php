7c7088bf0e26870d026c50047203796bb56e3364
Sean Davis <sean@sdavismedia.com>
2015-08-22 13:16:56 -0500

    adjust admin menu links

--- inc/options/admin-menu.php
+++ inc/options/admin-menu.php
@@ -5,11 +5,8 @@
  ***** ALL CUSTOM CODING SHOULD BE DONE IN A CHILD THEME.
  ***** MORE INFORMATION - http://volatylthemes.com/why-child-themes/
  *******************************************************************
- * 
- * Admin Menu (very top menu bar) Volatyl Options
  *
- * You can't tell me this isn't handy. If you create your own options
- * tab, hack into this array to add your options to the menu.
+ * Admin Menu (very top menu bar) Volatyl Options
  *
  * @since Volatyl 1.0
  */
@@ -24,30 +21,10 @@ if (get_theme_mod('volatyl_toolbar')) {
 			),
 		));
 		$admin_bar->add_menu(array(
-			'id'    		=> 'volatyl-options-global',
-			'parent' 		=> 'volatyl',
-			'title' 		=> __('Global Options', 'volatyl'),
-			'href'  		=> site_url('/wp-admin/themes.php?page=volatyl_options&tab=global'),
-			'meta'  		=> array(
-				'title' 	=> __('Global Options', 'volatyl'),
-				'target'	=> '_self'
-			),
-		));
-		$admin_bar->add_menu(array(
-			'id'			=> 'volatyl-options-content',
-			'parent' 		=> 'volatyl',
-			'title' 		=> __('Content Options', 'volatyl'),
-			'href'  		=> site_url('/wp-admin/themes.php?page=volatyl_options&tab=content'),
-			'meta'  		=> array(
-				'title' 	=> __('Content Options', 'volatyl'),
-				'target' 	=> '_self'
-			),
-		));
-		$admin_bar->add_menu(array(
 			'id'    		=> 'volatyl-options-hooks',
 			'parent' 		=> 'volatyl',
 			'title' 		=> __('Custom Hooks', 'volatyl'),
-			'href'  		=> site_url('/wp-admin/themes.php?page=volatyl_options&tab=hooks'),
+			'href'  		=> admin_url('themes.php?page=volatyl_options&tab=hooks'),
 			'meta'  		=> array(
 				'title' 	=> __('Custom Hooks', 'volatyl'),
 				'target' 	=> '_self'
@@ -57,7 +34,7 @@ if (get_theme_mod('volatyl_toolbar')) {
 			'id'    		=> 'volatyl-options-license',
 			'parent' 		=> 'volatyl',
 			'title' 		=> __('License Key', 'volatyl'),
-			'href'  		=> site_url('/wp-admin/themes.php?page=volatyl_options&tab=license'),
+			'href'  		=> admin_url('themes.php?page=volatyl_options&tab=license'),
 			'meta'  		=> array(
 				'title' 	=> __('License Key', 'volatyl'),
 				'target' 	=> '_self'
